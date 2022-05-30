533c9fd0dc8707e9e3d7cdccf3b79fe348e51773
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-05-30 17:36:17 +0430

    put and patch in mongoose

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
-const { default: axios } = require("axios");
-const path = require("path");
-const res = require("express/lib/response");
 const { userModel } = require("../models/user");
 const { hashString } = require("../modules/utils");
 const { isValidObjectId } = require("mongoose");
+const { object } = require("joi");
 
 router.post("/create", async (req, res, next) => {
   try {
@@ -104,6 +102,79 @@ router.delete("/:id", async (req, res, next) => {
       });
     }
     throw { status: 400, message: "کاربر حذف نشد" };
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+
+    if (!isValidObjectId(id)) {
+      throw { status: 400, message: "شناسه ارسال شده درست نمیباشد" };
+    }
+    const userFindResult = await userModel.findById(id);
+    if (!userFindResult) {
+      throw { status: 404, message: "کاربری با این مشخصات وجود نداشت" };
+    }
+
+    const { username, mobile, email } = req.body;
+    let data = { ...req.body };
+    let user;
+
+    // TODO : این کد ولیدیشن ایمیل و شماره تماس میباشد
+    const mobileRegexp = /^09[0-9]{9}/;
+    const emailRegexp = /^[a-z]+[a-z0-9\_\.]{5,}\@[a-z]{2,8}\.[a-z]{2,8}/;
+    if (mobile && !mobileRegexp.test(mobile)) {
+      throw { status: 400, message: "شماره موبایل وارد شده اشتباه است" };
+    }
+    if (email && !emailRegexp.test(email)) {
+      throw { status: 400, message: "ایمیل وارد شده اشتباه است" };
+    }
+
+    // TODO : این کد برای ولیدیشن یوزر تکراری میباشد
+    if (username) {
+      user = await userModel.findOne({ username });
+    }
+    if (user) {
+      throw { status: 400, message: "نام کاربری قبلا استفاده شده است" };
+    }
+    if (email) {
+      user = await userModel.findOne({ email });
+    }
+    if (user) {
+      throw { status: 400, message: "ایمیل قبلا استفاده شده است" };
+    }
+    if (mobile) {
+      user = await userModel.findOne({ mobile });
+    }
+    if (user) {
+      throw { status: 400, message: "شماره موبایل قبلا استفاده شده است" };
+    }
+
+    Object.entries(data).forEach(([key, value]) => {
+      if (
+        !value ||
+        ["", " ", ".", null, undefined].includes(value) ||
+        value.length < 3
+      ) {
+        delete data[key];
+      }
+      if (!["username", "email", "mobile"].includes(key)) {
+        delete data[key];
+      }
+    });
+
+    const result = await userModel.updateOne({ _id: id }, { ...data });
+    if (result.modifiedCount > 0) {
+      return res.json({
+        status: 200,
+        success: true,
+        message: "کاربر با موفقیت بروز شد",
+      });
+    }
+    throw { status: 500, message: "کاربر بروز نشد" };
   } catch (error) {
     next(error);
   }
